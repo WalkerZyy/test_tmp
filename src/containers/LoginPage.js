3aaa345e383f3ad1587dd5bed303c493a1be6ddd
zyy <1073040046@qq.com>
2018-06-22 15:37:59 +0800

    dynamic-imports

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -2,12 +2,15 @@ import React from 'react';
 import gql from "graphql-tag";
 import { Layout } from 'antd';
 
-export default class LoginPage extends React.PureComponent{
+class LoginPage extends React.PureComponent{
 
     render(){
         return (
             <Layout>
+                loginPage
             </Layout>
         ); 
     }
 }
+
+export default LoginPage;
--- src/containers/RootContainer.js
+++ src/containers/RootContainer.js
@@ -4,14 +4,18 @@ import {
   Route,
   Link
 } from 'react-router-dom'
+import AsyncComponent from '../AsyncComponent';
 import HomePage from './HomePage'
 // import TestPage from './TestPage'
 // import TestLocalPage from './TestLocalPage'
+
+const LoginPage = AsyncComponent(() => import("./LoginPage"));
 
 export const Routes = () => (
   <React.Fragment>
     <Route exact path="/" component={HomePage}/>
     <Route path="/home/tk/:token" component={HomePage}/>
+    <Route path="/login" component={LoginPage}/>
     {/* <Route path="/test" component={TestPage}/>
     <Route path="/test_local" component={TestLocalPage}/> */}
   </React.Fragment>
@@ -22,6 +26,7 @@ const RootContainer = () => (
     <div>
       <ul>
         <li><Link to="/">Home</Link></li>
+        <li><Link to="/login">Login</Link></li>
         {/* <li><Link to="/test">TestPage</Link></li>
         <li><Link to="/test_local">TestLocalPage</Link></li> */}
       </ul>
