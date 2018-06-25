ee3790f292897d153e372fb5df436475fecc22af
zyy <1073040046@qq.com>
2018-06-25 17:07:35 +0800

    update fix config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -94,8 +94,8 @@ module.exports = {
       // add by zyy
       '@src': path.resolve(__dirname, '../src'),
       '@less': path.resolve(__dirname, '../src/less'),
-      '@components': path.resolve(__dirname, '..src/components'),
-      '@container': path.resolve(__dirname, '..src/container'),
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@container': path.resolve(__dirname, '../src/container'),
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
